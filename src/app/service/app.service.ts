2f9deac0c93053f0a1df7b8612c14445def0e96f
ericyu049 <ericyu049@gmail.com>
2021-04-29 13:42:33 -0400

    viewer switch

--- src/app/service/app.service.ts
+++ src/app/service/app.service.ts
@@ -3,28 +3,29 @@ import { Injectable } from "@angular/core";
 
 @Injectable()
 export class AppService {
+    HOSTNAME_URL = 'https://www.googleapis.com/youtube/v3';
     constructor(private http: HttpClient) {
 
     }
     searchVideos(query) {
-        console.log('hiiii');
-        const url = "/search";
-        // return this.http.get(url, { params: query, reportProgress: true, responseType: "json" });
-        return this.http.get('/sakamichitv/assets/nogi-main.json',  { params: query, reportProgress: true, responseType: "json" });
+        const url = this.HOSTNAME_URL +  "/search";
+        return this.http.get(url, { params: query, reportProgress: true, responseType: "json" });
+        // return this.http.get('/sakamichitv/assets/nogi-main.json',  { params: query, reportProgress: true, responseType: "json" });
     }
     getVideoStats(query) {
-        const url = "/videos";
-        // return this.http.get(url, { params: query, reportProgress: true, responseType: "json" });
-        return this.http.get('/sakamichitv/assets/stats.json',  { params: query, reportProgress: true, responseType: "json" });
+        const url = this.HOSTNAME_URL + "/videos";
+        return this.http.get(url, { params: query, reportProgress: true, responseType: "json" });
+        // return this.http.get('/sakamichitv/assets/stats.json',  { params: query, reportProgress: true, responseType: "json" });
 
     }
     getChannelDetail(id) {
-        const url = "/channels";
+        const url = this.HOSTNAME_URL + "/channels";
         const query = {
             key: 'AIzaSyAzdHWiPLODTady7NNa4zben6MOXoRpwd4',
             part: 'statistics, snippet',
             id: id
         }
-        return this.http.get('/sakamichitv/assets/channel.json',  { params: query, reportProgress: true, responseType: "json" });
+        return this.http.get(url, { params: query, reportProgress: true, responseType: "json" });
+        // return this.http.get('/sakamichitv/assets/channel.json',  { params: query, reportProgress: true, responseType: "json" });
     }
 }
